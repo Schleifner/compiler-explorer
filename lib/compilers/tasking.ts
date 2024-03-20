b950651a445b4df116c2f4b8c268984d9f552907
Huang Yiyang <yy_huang95@163.com>
2024-03-20 17:48:35 +0800

    reconstructed tasking asm parser and elf parser

--- lib/compilers/tasking.ts
+++ lib/compilers/tasking.ts
@@ -1,16 +1,52 @@
+import * as fsPromise from 'fs/promises';
 import path from 'path';
-import {fileURLToPath} from 'url';
 
-import {CompilationResult, ExecutionOptions} from '../../types/compilation/compilation.interfaces';
+import {ExecutionOptions} from '../../types/compilation/compilation.interfaces';
 import {CompilerInfo} from '../../types/compiler.interfaces';
+import {BasicExecutionResult, UnprocessedExecResult} from '../../types/execution/execution.interfaces';
 import {CompilerOutputOptions, ParseFiltersAndOutputOptions} from '../../types/features/filters.interfaces';
+import {ResultLine} from '../../types/resultline/resultline.interfaces';
 import {BaseCompiler} from '../base-compiler';
 import {CompilationEnvironment} from '../compilation-env';
 import {AsmParserTasking} from '../parsers/asm-parser-tasking';
+import * as utils from '../utils';
+
+function parseSeverity(message: string): number {
+    if (message.startsWith('W')) return 2;
+    if (message.startsWith('E')) return 3;
+    return 1;
+}
+
+function parseOutput(output: string, inputFilename?: string): ResultLine[] {
+    const ansiColoursRe = /\x1B\[[\d;]*[Km]/g;
+    const sourceRe = /\s+(?<level>\w+):\s*\["<source>"\s*(?<lineno>\d+)\/(?<column>\d+)]\s+(?<msg>.+)/;
+    const result: ResultLine[] = [];
+    utils.eachLine(output, line => {
+        if (inputFilename) line = line.split(inputFilename).join('<source>');
+        if (line !== null) {
+            const lineObj: ResultLine = {text: line};
+            const filteredline = line.replace(ansiColoursRe, '');
+            const match = filteredline.match(sourceRe);
+            if (match && match.groups) {
+                const message = match.groups.msg.trim();
+                lineObj.tag = {
+                    line: parseInt(match.groups.lineno),
+                    column: parseInt(match.groups.column || '0'),
+                    text: message,
+                    severity: parseSeverity(match.groups.level),
+                    file: inputFilename ? path.basename(inputFilename) : undefined,
+                };
+            }
+            result.push(lineObj);
+        }
+    });
+    return result;
+}
 
 export class TaskingCompiler extends BaseCompiler {
     override asm: AsmParserTasking;
     filtersBinary: boolean;
+    compileOptions: Array<string>;
 
     static get key() {
         return 'tasking';
@@ -19,78 +55,32 @@ export class TaskingCompiler extends BaseCompiler {
     constructor(info: CompilerInfo, env: CompilationEnvironment) {
         super(info, env);
         this.asm = new AsmParserTasking(this.compilerProps);
-        this.compiler.exe = this.compiler.exe.replace('cctc.exe', 'hldumptc.exe');
+        // this.compiler.exe = this.compiler.exe.replace(/c[cp]tc\.exe/, 'hldumptc.exe');
+        this.compileOptions = ['-g', '--core=tc1.8', '-c', '-O0'];
+        if (info.lang === 'c++') {
+            this.compileOptions.push('--force-c++', '--pending-instantiations=200');
+        }
     }
 
     override optionsForFilter(filters, outputFilename) {
-        //hldumptc -cc -FCdFHMNSY -is $(OUTPATH)/$< -o $(OUTPATH)/cppdemo.asm
-        if (filters.binaryObject) this.filtersBinary = true;
-        else this.filtersBinary = false;
-
-        return ['-cc', '-FCdFHMNSY', '-o', this.filename(outputFilename)];
+        if (!filters.binary && !filters.binaryObject) {
+            filters.binaryObject = true;
+            filters.binary = true;
+        }
+        return this.compileOptions.concat(['-o', outputFilename]);
     }
 
-    override async runCompiler(
-        compiler: string,
-        options: string[],
-        inputFilename: string,
-        execOptions: ExecutionOptions,
-    ) {
-        if (!execOptions) {
-            execOptions = this.getDefaultExecOptions();
-        }
-
-        execOptions.customCwd = path.dirname(inputFilename);
-
-        const index = compiler.indexOf('hldumptc.exe');
-        const compilertasking = compiler.slice(0, index) + 'cctc.exe';
-        let optionstasking = new Array<string>();
-        const sourceFile = options[options.length - 1];
-        const objectFile = sourceFile.replace(/\.cpp|\.c/g, '.o');
-        if (sourceFile.endsWith('.cpp')) {
-            optionstasking = [
-                '-g',
-                '--core=tc1.8',
-                '--force-c++',
-                '--pending-instantiations=200',
-                '-c',
-                '-O0',
-                '-o',
-                objectFile,
-                sourceFile,
-            ];
-        } else {
-            optionstasking = ['--core=tc1.8', '-c', '-g', '-o', objectFile, sourceFile];
-        }
-        if (options.length > 5) {
-            optionstasking = optionstasking.concat(options.slice(4, -1));
-        }
-
-        const result1 = await this.exec(compilertasking, optionstasking, execOptions);
-
-        if (this.filtersBinary && inputFilename.endsWith('.cpp')) {
-            const file = path.dirname(fileURLToPath(import.meta.url)) + '\\..\\..\\tc49x.lsl';
-            optionstasking = [
-                '--force-c++',
-                '--pending-instantiations=200',
-                file,
-                '--lsl-core=tc0',
-                '-o',
-                objectFile,
-                objectFile,
-            ];
-            const result2 = await this.exec(compilertasking, optionstasking, execOptions);
-            if (result2.code !== 0) options[4] = "Linkerror: can't generate .exe file.";
-        } else if (this.filtersBinary && inputFilename.endsWith('.c')) {
-            const file = path.dirname(fileURLToPath(import.meta.url)) + '\\..\\..\\tc49x.lsl';
-            optionstasking = ['-d', file, '--lsl-core=tc0', '-o', objectFile, objectFile];
-            const result2 = await this.exec(compilertasking, optionstasking, execOptions);
-            if (result2.code !== 0) options[4] = "Linkerror: can't generate .exe file.";
-        }
-        options.length = 4;
-        options.push(objectFile);
-        this.asm._elffilepath = objectFile;
-        return super.runCompiler(compiler, options, inputFilename, execOptions);
+    override processExecutionResult(input: UnprocessedExecResult, inputFilename?: string): BasicExecutionResult {
+        const start = performance.now();
+        const stdout = parseOutput(input.stdout, inputFilename);
+        const stderr = parseOutput(input.stderr, inputFilename);
+        const end = performance.now();
+        return {
+            ...input,
+            stdout,
+            stderr,
+            processExecutionResultTime: end - start,
+        };
     }
 
     override preProcess(source: string, filters: CompilerOutputOptions): string {
@@ -104,7 +94,47 @@ export class TaskingCompiler extends BaseCompiler {
         return [];
     }
 
-    override supportsObjdump(): boolean {
-        return false;
+    async disassembleElf(outputFilename: string): Promise<string> {
+        outputFilename = this.filename(outputFilename);
+        const options = ['-cc', '-FCdFHMNSY', outputFilename];
+        const execOptions: ExecutionOptions = {
+            maxOutput: this.env.ceProps('max-asm-size', 64 * 1024 * 1024),
+            customCwd: path.dirname(outputFilename),
+        };
+        const hldumptc = path.join(path.dirname(this.compiler.exe), 'hldumptc.exe');
+        const objResult = await this.exec(hldumptc, options, execOptions);
+        if (objResult.code === 0) {
+            return objResult.stdout;
+        } else {
+            return '';
+        }
+    }
+
+    override async objdump(
+        outputFilename: string,
+        result: any,
+        maxSize: number,
+        intelAsm: boolean,
+        demangle: boolean,
+        staticReloc: boolean,
+        dynamicReloc: boolean,
+        filters: ParseFiltersAndOutputOptions,
+    ) {
+        const start = performance.now();
+        outputFilename = this.getObjdumpOutputFilename(outputFilename);
+
+        if (!(await utils.fileExists(outputFilename))) {
+            result.asm = '<No output file ' + outputFilename + '>';
+            return result;
+        }
+        const elf = await fsPromise.readFile(outputFilename);
+        const asm = await this.disassembleElf(outputFilename);
+        const end = performance.now();
+        result.objdumpTime = (end - start).toString();
+        result.asm = JSON.stringify({
+            elf: elf,
+            asm: asm,
+        });
+        return result;
     }
 }
--- lib/parsers/asm-parser-tasking.ts
+++ lib/parsers/asm-parser-tasking.ts
@@ -6,265 +6,114 @@ import {
 } from '../../types/asmresult/asmresult.interfaces';
 import {ParseFiltersAndOutputOptions} from '../../types/features/filters.interfaces';
 import {PropertyGetter} from '../properties.interfaces';
-import {elf_Parse} from '../tooling/tasking-elfparse-tool';
+import {Elf32DebugLineSection, Elf32Parser} from '../tooling/tasking-elfparse-tool';
 import * as utils from '../utils';
 
 import {AsmParser} from './asm-parser';
 import {IAsmParser} from './asm-parser.interfaces';
 import {AsmRegex} from './asmregex';
 
+class AsmTextSection {
+    section: string;
+    lines: Array<ParsedAsmResultLine>;
+    constructor(section: string) {
+        this.lines = [];
+        this.section = section;
+    }
+
+    addAsm(addr: string, opcode: string, operands: string) {
+        const line: ParsedAsmResultLine = {
+            text: operands,
+            opcodes: opcode.length === 0 ? undefined : opcode.trim().split(' '),
+            address: parseInt(addr, 16),
+            source: null,
+        };
+        this.lines.push(line);
+    }
+
+    attachDebugLine(debugLineSec: Elf32DebugLineSection) {
+        const offset = debugLineSec.relaOffset;
+        let lineno = 0;
+        while (lineno < this.lines.length) {
+            const line = this.lines[lineno];
+            for (const debugLine of debugLineSec.decodedLines) {
+                if (debugLine.opaddr + offset === line.address) {
+                    line.source = {
+                        file: '',
+                        mainsource: true,
+                        line: debugLine.line,
+                    };
+                }
+            }
+            lineno++;
+        }
+    }
+}
+
 export class AsmParserTasking extends AsmParser implements IAsmParser {
-    taskingText: RegExp;
-    taskingMachineCode: RegExp;
+    sectionRe: RegExp;
+    asmRe: RegExp;
     _elffilepath: string;
     testcpppath: string;
 
     constructor(compilerProps?: PropertyGetter) {
         super(compilerProps);
-        this.taskingText = /^\s+(.sect|.sdecl)\s+'(.*)'.*/;
-        this.taskingMachineCode = /(^\w+)((?:\s*(?:\d|[a-f]){2}){2,4})\s+(.*:)?(\s*(.*)\s+(.*))/;
+        this.sectionRe = /^\s*.sect\s+'(?<sect>.+)'/;
+        this.asmRe = /(?<addr>[\da-f]{8})\s+(?<code>(?:[\da-f]{2}\s){1,4})\s+(?:\S*:)*\s*(?<opcode>.+)/;
     }
 
     override processAsm(asmResult: string, filters: ParseFiltersAndOutputOptions): ParsedAsmResult {
-        if (filters.binaryObject) return this.processBinaryAsm(asmResult, filters);
+        if (asmResult === '<Compilation failed>') {
+            return {
+                parsingTime: '0',
+                asm: [
+                    {
+                        text: asmResult,
+                    },
+                ],
+            };
+        }
         const startTime = process.hrtime.bigint();
-        const asm: ParsedAsmResultLine[] = [];
         const labelDefinitions: Record<string, number> = {};
-
-        const asmLines = utils.splitLines(asmResult);
-        const startingLineCount = asmLines.length;
-
-        let source: AsmResultSource | undefined | null;
-
-        function maybeAddBlank() {
-            const lastBlank = asm.length === 0 || asm[asm.length - 1].text === '';
-            if (!lastBlank) asm.push({text: '', source: null, labels: []});
-        }
-
-        let filetext = '';
-        let address = '';
-
-        const elfParseTool = new elf_Parse(this._elffilepath);
-        if (this.testcpppath) {
-            elfParseTool._elf_examplepathcpp = this.testcpppath;
-        }
-        elfParseTool.start();
-
-        for (let line of asmLines) {
-            if (line.trim() === '') {
-                maybeAddBlank();
+        const raw = JSON.parse(asmResult);
+        const asmContent: string = raw.asm;
+        const elfContent: Buffer = Buffer.from(raw.elf);
+        const asmLines = utils.splitLines(asmContent);
+        const textSecs: Array<AsmTextSection> = [];
+        let asm: ParsedAsmResultLine[] = [];
+        let match;
+        for (const line of asmLines) {
+            if (line.length === 0) {
                 continue;
-            }
-
-            const match = null;
-            line = utils.expandTabs(line);
-
-            const matchtext = line.match(this.taskingText);
-            if (matchtext) {
-                if (matchtext[1] === '.sect') {
-                    filetext = matchtext[2];
-                    if (elfParseTool._elf_debugLineSet.has(filetext) || filters.libraryCode) {
-                        asm.push({
-                            text: filetext,
-                        });
-                    }
-                }
-                continue;
-            }
-
-            const matchaddr = line.match(this.taskingMachineCode);
-            if (matchaddr) {
-                address = matchaddr[1];
-                if (matchaddr[5].includes('call') || matchaddr[5].trim() === 'j') {
-                    if (matchaddr[1] === '00000000') {
-                        const map = elfParseTool._elf_RelaMap.get(filetext);
-                        if (map) {
-                            const value = map.get(0);
-                            if (value) {
-                                matchaddr[4] = ' ' + matchaddr[5] + value;
-                            }
-                        }
-                    } else {
-                        const key = parseInt(matchaddr[6]);
-                        const map = elfParseTool._elf_RelaMap.get(filetext);
-                        if (map) {
-                            const value = map.get(key);
-                            if (value) {
-                                matchaddr[4] = ' ' + matchaddr[5] + value;
-                            }
-                        }
-                    }
-                }
-                if (filters.directives) {
-                    line = '  ' + matchaddr[4].trim();
-                }
-            }
-
-            const text = AsmRegex.filterAsmLine(line, filters);
-
-            const labelsInLine = match ? [] : this.getUsedLabelsInLine(text);
-
-            const map = elfParseTool._elf_debugLineMap.get(filetext);
-            if (map) {
-                const _linenumber: number | undefined = map.get(address);
-                if (_linenumber) {
-                    source = {
-                        column: 1,
-                        file: null,
-                        line: _linenumber,
-                    };
-                }
-            }
-
-            if (elfParseTool._elf_debugLineSet.has(filetext) || filters.libraryCode) {
-                if (elfParseTool._elf_debugLineSet.has(filetext)) {
-                    asm.push({
-                        text: text,
-                        source: source,
-                        labels: labelsInLine,
-                    });
+            } else if ((match = line.match(this.sectionRe))) {
+                textSecs.push(new AsmTextSection(match.groups.sect));
+            } else if ((match = line.match(this.asmRe))) {
+                const textSec = textSecs[textSecs.length - 1];
+                if (filters.binary && filters.binaryObject) {
+                    textSec.addAsm(match.groups.addr, '', match.groups.opcode);
                 } else {
-                    asm.push({
-                        text: text,
-                        source: null,
-                        labels: labelsInLine,
-                    });
+                    textSec.addAsm(match.groups.addr, match.groups.code, match.groups.opcode);
                 }
             }
         }
-
+        const elfParser = new Elf32Parser(elfContent);
+        for (const sec of textSecs) {
+            for (const debugLineSec of elfParser.debugLines) {
+                if (debugLineSec.relaSection === sec.section) {
+                    sec.attachDebugLine(debugLineSec);
+                }
+            }
+        }
+        for (const sec of textSecs) {
+            asm.push({text: `${sec.section}:`});
+            asm = asm.concat(sec.lines);
+        }
         const endTime = process.hrtime.bigint();
         return {
             asm: asm,
             labelDefinitions: labelDefinitions,
             parsingTime: ((endTime - startTime) / BigInt(1000000)).toString(),
-            filteredCount: startingLineCount - asm.length,
-        };
-    }
-
-    override processBinaryAsm(asmResult: string, filters: ParseFiltersAndOutputOptions): ParsedAsmResult {
-        const startTime = process.hrtime.bigint();
-        const asm: ParsedAsmResultLine[] = [];
-        const labelDefinitions: Record<string, number> = {};
-
-        const asmLines = utils.splitLines(asmResult);
-        const startingLineCount = asmLines.length;
-
-        let source: AsmResultSource | undefined | null;
-
-        function maybeAddBlank() {
-            const lastBlank = asm.length === 0 || asm[asm.length - 1].text === '';
-            if (!lastBlank) asm.push({text: '', source: null, labels: []});
-        }
-
-        const elfParseTool = new elf_Parse(this._elffilepath);
-        if (this.testcpppath) {
-            elfParseTool._elf_examplepathcpp = this.testcpppath;
-        }
-        elfParseTool.start();
-
-        let cnt = 0;
-        for (const line of elfParseTool._elf_section) {
-            if (line.sh_strname === '.debug_line') {
-                elfParseTool._elf_section[cnt].sh_end =
-                    elfParseTool._elf_section[cnt].sh_offset + elfParseTool._elf_section[cnt].sh_size - 1;
-                elfParseTool.parse_debugLine(cnt, '');
-            }
-            cnt++;
-        }
-
-        let map = elfParseTool._elf_debugLineMap.get(elfParseTool._elf_examplepathcpp);
-
-        if (!map) {
-            map = elfParseTool._elf_debugLineMap.get(elfParseTool._elf_examplepathc);
-        }
-
-        let minaddress = '';
-        let maxaddress = '';
-
-        if (map) {
-            minaddress = '99999999';
-            for (const key of map.keys()) {
-                if (key < minaddress && key !== '00000000') {
-                    minaddress = key;
-                }
-                if (key > maxaddress) {
-                    maxaddress = key;
-                }
-            }
-        }
-
-        let completeTag1 = false;
-        let completeTag2 = false;
-
-        for (let line of asmLines) {
-            let address = '';
-            if (line.trim() === '') {
-                maybeAddBlank();
-                continue;
-            }
-
-            const match = null;
-            line = utils.expandTabs(line);
-
-            const matchaddr = line.match(this.taskingMachineCode);
-            if (matchaddr) {
-                completeTag1 = true;
-                address = matchaddr[1];
-                if (filters.directives) {
-                    line = '  ' + matchaddr[4].trim();
-                }
-            } else {
-                completeTag1 = false;
-                completeTag2 = false;
-            }
-
-            const text = AsmRegex.filterAsmLine(line, filters);
-
-            const labelsInLine = match ? [] : this.getUsedLabelsInLine(text);
-
-            if (address >= minaddress && address <= maxaddress) {
-                if (map) {
-                    completeTag2 = true;
-                    const _linenumber: number | undefined = map.get(address);
-                    if (_linenumber) {
-                        source = {
-                            column: 1,
-                            file: null,
-                            line: _linenumber,
-                        };
-                    }
-                    asm.push({
-                        text: text,
-                        source: source,
-                        labels: labelsInLine,
-                    });
-                }
-                continue;
-            } else {
-                if (filters.libraryCode) {
-                    asm.push({
-                        text: text,
-                        source: null,
-                        labels: labelsInLine,
-                    });
-                }
-                if (completeTag1 && completeTag2) {
-                    asm.push({
-                        text: text,
-                        source: source,
-                        labels: labelsInLine,
-                    });
-                }
-            }
-        }
-
-        const endTime = process.hrtime.bigint();
-        return {
-            asm: asm,
-            labelDefinitions: labelDefinitions,
-            parsingTime: ((endTime - startTime) / BigInt(1000000)).toString(),
-            filteredCount: startingLineCount - asm.length,
+            filteredCount: asmLines.length - asm.length,
         };
     }
 }
